1c5d1f79e2265ef5b9c2e51dd78b46be462ff935
iurych <iury.vibe@gmail.com>
2024-02-08 12:14:46 -0300

    feat: set basic config styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 import App from './App.tsx'
+import { GlobalStyle } from './global.ts'
 import { defaultTheme } from './styles/themes/default.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider  theme={{ defaultTheme }}>
+    <ThemeProvider  theme={ defaultTheme }>
+      <GlobalStyle />
       <App />
     </ThemeProvider>
   </React.StrictMode>,
