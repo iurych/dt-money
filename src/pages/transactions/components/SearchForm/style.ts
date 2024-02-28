ce489b52012246436f579b6d145d7224372e6d28
iurych <iury.vibe@gmail.com>
2024-02-28 00:57:18 -0300

    feat: change search btn style to not allowed state

--- src/pages/transactions/components/SearchForm/style.ts
+++ src/pages/transactions/components/SearchForm/style.ts
@@ -32,8 +32,12 @@ export const SearchFormContainer = styled.form`
     border-radius: 6px;
     cursor: pointer;
 
+    &:disabled {
+      opacity: 0.6;
+      cursor: not-allowed;
+    }
 
-    &:hover {
+    &:not(:disabled):hover {
       background: ${(props) => props.theme['green-500']};
       border-color: ${(props) => props.theme['green-500']};
       color: ${(props) => props.theme.white};
