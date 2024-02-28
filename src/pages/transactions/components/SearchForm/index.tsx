2d489bb63c654ca5e5329c0ea95bd826f14b4226
iurych <iury.vibe@gmail.com>
2024-02-28 00:54:14 -0300

    feat: add hook-form to search form

--- src/pages/transactions/components/SearchForm/index.tsx
+++ src/pages/transactions/components/SearchForm/index.tsx
@@ -1,11 +1,42 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { MagnifyingGlass } from "phosphor-react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
 import { SearchFormContainer } from "./style";
 
+const SearchFormSchema = z.object({
+  query: z.string()
+})
+
+type SearchFormType = z.infer<typeof SearchFormSchema>
+
 export const SearchForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting }
+  } = useForm<SearchFormType>({
+    resolver: zodResolver(SearchFormSchema)
+  })
+
+  const handleOnSubmit = async (data: SearchFormType) => {
+    await new Promise(resolve => setTimeout(resolve, 2000))
+
+    console.log(data)
+  }
   return (
-    <SearchFormContainer>
-      <input type="text" placeholder="Search for a transaction..." />
-      <button type="submit"> <MagnifyingGlass /> Search </button>
+    <SearchFormContainer onSubmit={handleSubmit(handleOnSubmit)} >
+      <input 
+        type="text" 
+        placeholder="Search for a transaction..."
+        {...register("query", { required: true })}
+      />
+      <button 
+        type="submit"
+        disabled={isSubmitting}
+      > 
+        <MagnifyingGlass /> Search 
+      </button>
     </SearchFormContainer>
   );
 }
