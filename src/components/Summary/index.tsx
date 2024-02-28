946fc2204f6db2ebd1d4074a97d081858395f036
iurych <iury.vibe@gmail.com>
2024-02-28 00:08:10 -0300

    feat: create useSummary hook

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,30 +1,11 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react"
-import { useContext } from "react"
-import { TransactionsContext } from "../../contexts/TransactionsContext"
+import { useSummary } from "../../hooks/useSummary"
 import { formatCurrency } from "../../utils/formatter"
 import { SummaryCard, SummaryContainer } from "./style"
 
 export const Summary = () => {
 
-  const { transactions } = useContext(TransactionsContext)
-
-  const summarized = transactions.reduce((acc, transaction) => {
-    if (transaction.type === "income") {
-      acc.income += transaction.price
-      acc.total += transaction.price
-    } else {
-      acc.outcome += transaction.price
-      acc.total -= transaction.price
-    }
-    return acc
-  },
-  {
-    income: 0,
-    outcome: 0,
-    total: 0
-  }
-  )
-
+  const summarized = useSummary()
   
   return (
     <SummaryContainer>
@@ -50,7 +31,7 @@ export const Summary = () => {
       <SummaryCard variant="green" >
         <header>
           <span>Total</span>
-          <CurrencyDollar size={32} color="#fff" />
+          <CurrencyDollar size={32} color="#ffffffd3" />
         </header>
 
         <strong>{formatCurrency.format(summarized.total)}</strong>
